e6768ccc8cf53b3af5f866333e70358de222782a
nugod <mmowfug@yahoo.com>
2020-05-22 16:10:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//un tableau contenant les sources des images
+var imgSrc = ['./img/slide1.jpg', './img/slide2.jpg', './img/slide3.jpg', './img/slide4.jpg', './img/slide5.jpg'];
+var img = document.querySelector('#image'); //récupérer la balise d'image par l'id
+var i = 0; //identifier le compteur
+
+// la fonction qui montre l'image suivante
+function next() {
+  if (i < imgSrc.length - 1) { // si le compteur n'est pas arrivé à la dernière image
+    i++;                       // ajouter un au compteur pour passer à l'image suivante
+    img.src = imgSrc[i];       // changer la source d'image en fonction du compteur
+  } else {
+    i = 0;     // Si le compteur atteint la dernière image, il recommence en mettant la valeur à zéro
+    img.src = imgSrc[i]; 
+  } 
+}
+
+// la fonction qui montre l'image précédente  
+function prev() {
+  if(i <= 0){ i = imgSrc.length; } //Si le compteur atteint la première image, il recommence en mettant la valeur au max
+  i--;                  // faire tomber un du compteur pour passer à l'image précédente
+  img.src = imgSrc[i];  // changer la source d'image en fonction du compteur
+}  
+
+var timer = setInterval(next, 4000); //  le défilement automatique
